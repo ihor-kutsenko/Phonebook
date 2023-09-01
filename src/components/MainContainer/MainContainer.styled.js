404c3c9893f5679f6552019679a9c80a36bbc612
Rammbest <igor-kutsenko@ukr.net>
2023-09-02 01:28:12 +0300

    add styled Home page

--- src/components/MainContainer/MainContainer.styled.js
+++ src/components/MainContainer/MainContainer.styled.js
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding: 24px;
   max-width: 100%;
+  min-height: 100vh;
+
   background-color: var(--background-color);
 `;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,14 @@
+import './Home.css';
+
 const Home = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <div className="t">
+      <div id="bubbles"></div>
+      <div id="bubbles2"></div>
+      <div id="bubbles3"></div>
+      <p className="title_home">PHONEBOOK</p>
+    </div>
+  );
 };
 
 export default Home;
